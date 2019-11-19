0d06ffd79d43cd25563b9766b90b1ce380deb20f
mLn023 <123Poney!>
2019-11-19 22:28:12 +0100

    interface grahpique BN presque jouable

--- Mini_Jeu/src/fr/projet/bataillenavale/GrilleBN.java
+++ Mini_Jeu/src/fr/projet/bataillenavale/GrilleBN.java
@@ -1,4 +1,5 @@
 package fr.projet.bataillenavale;
+
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.Parent;
@@ -10,13 +11,15 @@ import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
-public class GrilleBN extends Parent {
-	public int nbbateau = 5;
+class GrilleBN extends Parent {
+	int nbbateau = 5;
 	private VBox lignes = new VBox();
 	private boolean adversaire = false;
+	private Properties FXMLLoader;
 
-	public GrilleBN(boolean adversaire, EventHandler<? super MouseEvent> handler) {
+	GrilleBN(boolean adversaire, EventHandler<? super MouseEvent> handler) {
 		this.adversaire = adversaire;
 		for (int y = 0; y < 10; y++) {
 			HBox ligne = new HBox();
@@ -32,20 +35,15 @@ public class GrilleBN extends Parent {
 		getChildren().add(lignes);
 	}
 
-	public int getBateau(){
-		return nbbateau;
-	}
-
-	public void setBateau(int nbbateau){
-		this.nbbateau = nbbateau;
-	}
 
 	private boolean VerifPlacementBateau(Bateau B, int x, int y) {
-		int taille = B.getTaille();
-		if (!B.getHorizontal()) {
+		int taille = B.taille;
+
+		if (!B.horizontal) {
 			for (int i = y; i < y + taille; i++){
 				if (!PointExiste(x,i))
 					return false;
+
 				CaseBN box = getCase(x,i);
 				if (box.bateau != null)
 					return false;
@@ -61,6 +59,7 @@ public class GrilleBN extends Parent {
 			for (int i = x; i< x + taille;i++){
 				if (!PointExiste(i,y))
 					return false;
+
 				CaseBN box = getCase(i,y);
 				if (box.bateau != null)
 					return false;
@@ -68,12 +67,47 @@ public class GrilleBN extends Parent {
 				for (CaseBN autour : getAutour(i,y)){
 					if (!PointExiste(i,y))
 						return false;
+
 					if (autour.bateau != null)
 						return false;
 				}
 			}
 		}
+
 		return true;
+	}
+
+
+	Boolean placerBateau(Bateau bateau, int x, int y) {
+		if(VerifPlacementBateau(bateau, x,y)){
+			int taille = bateau.taille;
+			if (!bateau.horizontal){
+
+				for(int i = y; i < y + taille; i++){
+					CaseBN box = getCase(x,i);
+					box.bateau = bateau;
+					if(!adversaire){
+
+						box.setFill(Color.GRAY);
+						box.setStroke(Color.BLACK);
+					}
+				}
+			} else {
+
+				for (int i = x; i< x + taille; i++){
+					CaseBN box = getCase(i,y);
+					box.bateau = bateau;
+					if(!adversaire){
+
+						box.setFill(Color.GRAY);
+						box.setStroke(Color.BLACK);
+					}
+				}
+			}
+
+			return true;
+		}
+		return false;
 	}
 
 	private boolean PointExiste (Point2D point){
@@ -84,36 +118,9 @@ public class GrilleBN extends Parent {
 		return x >= 0 && x < 10 && y >= 0 && y < 10;
 	}
 
-	public Boolean placerBateau(Bateau bateau, int x, int y) {
-		if(VerifPlacementBateau(bateau, x,y)){
-			int taille = bateau.getTaille();
 
-			if (!bateau.getHorizontal()){
-				for(int i = y; i < y + taille; i++){
-					CaseBN box = getCase(x,i);
-					box.bateau = bateau;
-					if(!adversaire){
-						box.setFill(Color.WHITE);
-						box.setStroke(Color.GREEN);
-					}
-				}
-			} else {
-				for (int i = x; i< x + taille; i++){
-					CaseBN box = getCase(i,y);
-					box.bateau = bateau;
-					if(!adversaire){
-						box.setFill(Color.WHITE);
-						box.setStroke(Color.GREEN);
-					}
-				}
-			}
 
-			return true;
-		}
-		return false;
-	}
-
-	public CaseBN getCase(int x, int y) {
+	CaseBN getCase(int x, int y) {
 		return (CaseBN) ((HBox) lignes.getChildren().get(y)).getChildren().get(x);
 	}
 
@@ -136,25 +143,25 @@ public class GrilleBN extends Parent {
 		return autour.toArray(new CaseBN[0]);
 	}
 
-	public class CaseBN extends Rectangle {
-		public int x, y;
-		private Bateau bateau = null;
-		public boolean utilise = false;
+	class CaseBN extends Rectangle {
+		int x, y;
+		Bateau bateau = null;
+		boolean utilise = false;
 		private GrilleBN grille;
 
-		public CaseBN(int x, int y, GrilleBN grille){
+		CaseBN(int x, int y, GrilleBN grille){
 			super(30, 30);
 			this.x = x;
 			this.y = y;
 			this.grille = grille;
-			setFill(Color.LIGHTGRAY);
+			setFill(Color.valueOf("#1C046B"));
 			setStroke(Color.BLACK);
 		}
 
-		public boolean tire(){
+		boolean tire(){
+
 			utilise = true;
 			setFill(Color.BLACK);
-
 			if(bateau != null){
 				bateau.toucher();
 				setFill(Color.RED);
@@ -163,15 +170,12 @@ public class GrilleBN extends Parent {
 				}
 				return true;
 			}
+
 			return false;
 		}
-		public int get_X(){
-			return x;
-		}
 
-		public int get_Y(){
-			return y;
-		}
 	}
-			
+
+
+
 	 }

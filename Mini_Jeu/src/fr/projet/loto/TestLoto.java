fa87907115338b21a4e0498d337dde90c8840266
Yann-Malaquin <55402516+Yann-Malaquin@users.noreply.github.com>
2019-12-10 14:13:17 +0100

    Update TestLoto.java

--- Mini_Jeu/src/fr/projet/loto/TestLoto.java
+++ Mini_Jeu/src/fr/projet/loto/TestLoto.java
@@ -2,13 +2,4 @@ package fr.projet.loto;
 
 public class TestLoto {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PartieLoto P = new PartieLoto(4);
-		P.Creationbouleloto();
-		//P.AffichageBoules();
-		P.CreationGrille();
-		P.distribution();
-		P.debut_partie();
-	}
 }

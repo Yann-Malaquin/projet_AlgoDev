8ffad4fa6b43b997ec9a0d039b2fff5bdc53f6a8
Yann-Malaquin <55402516+Yann-Malaquin@users.noreply.github.com>
2019-12-10 14:41:30 +0100

    remise a niveau

--- Mini_Jeu/src/fr/projet/MiniJeux.java
+++ Mini_Jeu/src/fr/projet/MiniJeux.java
@@ -2,23 +2,21 @@ package fr.projet;
 
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Scanner;
 
-import fr.projet.loto.PartieLoto;
-import fr.projet.poker.Carte;
-import fr.projet.poker.JoueurPoker;
-import fr.projet.sudoku.Sudoku;
-import fr.projet.poker.*;
+        import fr.projet.loto.PartieLoto;
+        import fr.projet.poker.Carte;
+        import fr.projet.poker.JoueurPoker;
+        import fr.projet.sudoku.Sudoku;
+        import fr.projet.poker.*;
 
 public class MiniJeux {
 
-  public static void main(String[] args) throws IOException {
-	  // TODO Auto-generated method stub
-
-	  MenuPrincipal e = new MenuPrincipal();
-	  e.affichageMenuPrincipal();
-  }
-} 
+    public static void main(String[] args) throws IOException {
+        MenuPrincipal e = new MenuPrincipal();
+        e.affichageMenuPrincipal();
+    }
+}
